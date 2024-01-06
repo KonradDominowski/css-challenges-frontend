d124bde202c2065cef25403c28b2e9b92eb35b6f
Konrad Dominowski <zehkan@gmail.com>
2024-01-06 23:20:39 +0100

    removed title css module

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,12 +1,16 @@
-import styles from "./Title.module.scss";
+"use client";
+
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 export default function Title() {
   return (
-    <div className={styles.title}>
-      <h1>
+    <Box mt={"2rem"} textAlign={"center"}>
+      <Heading as={"h1"} fontSize={"4.5rem"} lineHeight={"1"} fontWeight={400} letterSpacing={"-2px"}>
         Learn HTML and CSS <br></br>by <strong>practicing</strong>
-      </h1>
-      <p>Choose a topic you want to practice, or go ahead and do all challenges from start to finish</p>
-    </div>
+      </Heading>
+      <Text p={"5rem"}>
+        Choose a topic you want to practice, or go ahead and do all challenges from start to finish
+      </Text>
+    </Box>
   );
 }
